d9c70ffed463fd28107e395a3b095836677b8b9f
onlyman1 <bilolabdumalikov1@gmail.com>
2023-07-15 12:14:16 +0500

    update list and detail book pages, and list and detail books pages testcases

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,125 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from book.models import Book
+
+
+class BookTestCase(TestCase):
+    def test_list(self):
+        response = self.client.get(reverse('list'))
+
+        self.assertContains(response, 'There is no Books.')
+
+    def test_list_title(self):
+        Book.objects.create(title='Bolaligim', description='Description1', isbn='12345678')
+        Book.objects.create(title='Alpomish', description='Description2', isbn='12122121')
+        Book.objects.create(title='Xamza', description='Description3', isbn='1234545565')
+        Book.objects.create(title='Ikki eshik orasi', description='Description4', isbn='12786544')
+
+        response = self.client.get(reverse('list'))
+
+        books = Book.objects.all()
+
+        for i in books:
+            self.assertContains(response, i.title)
+
+    def test_detail(self):
+        book = Book.objects.create(title='Oq kema', description='Description1', isbn='1212121')
+
+        response = self.client.get(reverse('detail', kwargs={"id": book.id}))
+
+        self.assertContains(response, book.title.upper())
+        self.assertContains(response, book.description.upper())
+        self.assertContains(response, book.isbn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
