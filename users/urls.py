fed29ea05550208164e4f1b2b576d5026143166e
onlyman1 <bilolabdumalikov1@gmail.com>
2023-07-16 20:08:43 +0500

    profile pages and profile testcase

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView, RegisterView
+from .views import LoginView, RegisterView, ProfileView
 
 
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='signup'),
     path('login/', LoginView.as_view(), name='login'),
+    path('profile/', ProfileView.as_view(), name='profile' )
 ]
